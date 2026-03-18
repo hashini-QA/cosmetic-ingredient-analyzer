2b6f35e7dbe3e139198c88924dcfc1fc8bd4916b
Hashini Krishnamoorthy <hashinireddy246@gmail.com>
2026-03-18 15:53:37 -0400

    Delete db directory

--- db/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "sqlite:///beauty_analyzer.db"
-
-engine = create_engine(DATABASE_URL, echo=False, future=True)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
-
-Base = declarative_base()
